8e5e71f35c47e55f33ab41a7235290853f49b56b
nonologism <leandre.becq@protonmail.com>
2022-05-15 10:55:48 +0200

    added players & moves

--- src/games/icebreaker/GameInterface.java
+++ src/games/icebreaker/GameInterface.java
@@ -3,10 +3,41 @@ package games.icebreaker;
 import java.util.Set;
 
 public class GameInterface {
-    private
+    public static int SIZE = 5;
+
+    private Player red = new Player("red", SIZE);
+    private Player black = new Player("black", SIZE);
+    private Board board = new Board(SIZE);
+    private String _currentPlayer = "red";
+
 
     boolean move(String player, String move) {
-        return false;
+        int origin[] = stringToCoord(move.charAt(0) + move.charAt(1) + "");
+        int dest[] = stringToCoord(move.charAt(3) + move.charAt(4) + "");
+        if (!player.equals(_currentPlayer)) {
+            System.out.println("It is not " + player + "'s turn.");
+            return false;
+        }
+        
+        _currentPlayer = (_currentPlayer.equals("red")) ? "black" : "red";
+        return true;
+    }
+
+    /**
+     * @param x x coordinate
+     * @param y y coordinate
+     * @return 'A1'
+     */
+    String coordToString(int x, int y) {
+        return (char) (y + 'A') + "" + (x + 1);
+    }
+
+    /**
+     * @param move 'A1'
+     * @return [x, y]
+     */
+    int[] stringToCoord(String move) {
+        return new int[]{move.charAt(0) - 'A', move.charAt(1) - '1' + 1};
     }
 
     int getScore(String player) {
---
+++ src/games/icebreaker/Player.java
@@ -0,0 +1,20 @@
+package games.icebreaker;
+
+import java.util.Objects;
+
+public class Player {
+    public int[][] _boats;
+    public int _score;
+
+    public Player(String name, int size) {
+        _score = 0;
+        if (Objects.equals(name, "black")){
+            _boats = new int[][]{{size - 1, 0}, {0, size - 1}, {size - 1, 2 * (size - 1)}};
+            return;
+        } else if (Objects.equals(name, "red")) {
+            _boats = new int[][]{{0, 0}, {2 * (size - 1), size - 1}, {0, 2 * (size - 1)}};
+            return;
+        }
+        System.out.println("Wrong name");
+    }
+}
