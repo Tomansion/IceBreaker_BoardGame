eeff81ba15c41d40f3c00f2df0b46eb2551d176b
tom.mansion <tomansion@yahoo.fr>
2022-05-15 16:55:49 +0200

    Possible moves done

--- src/games/icebreaker/Board.java
+++ src/games/icebreaker/Board.java
@@ -3,25 +3,24 @@ package games.icebreaker;
 import java.util.Set;
 
 public class Board {
-    private int[][] _board;
+    private boolean[][] _board;
     private int _size;
 
     public Board(int size) {
         _size = size;
-        _board = new int[2 * size - 1][2 * size - 1];
+        _board = new boolean[2 * size - 1][2 * size - 1];
         resetBoard();
     }
 
-    public int get(int x, int y) {
+    public boolean get(int x, int y) {
         if(outOfBound(x, y)) {
             System.out.println("Coordinates out of bound");
-
-            return -1;
+            return false;
         }
         return _board[y][x];
     }
 
-    public void set(int x, int y, int value) {
+    public void set(int x, int y, boolean value) {
         if(outOfBound(x, y)) {
             System.out.println("Coordinates out of bound");
             return;
@@ -38,7 +37,16 @@ public class Board {
         for(int x = 0; x < 2 * _size - 1; x++) {
             for(int y = 0; y < 2 * _size - 1; y++) {
                 if(!outOfBound(x, y)) {
-                    _board[y][x] = 1;
+                    _board[y][x] = true;
+                }
+            }
+        }
+    }
+    public void empty() {
+        for(int x = 0; x < 2 * _size - 1; x++) {
+            for(int y = 0; y < 2 * _size - 1; y++) {
+                if(!outOfBound(x, y)) {
+                    _board[y][x] = false;
                 }
             }
         }
@@ -119,8 +127,90 @@ public class Board {
         }
         return neighbors;
     }
+
+
     // Possible moves
-    // Return a set of coordinates (x, y) int
-    // From a boat position and a player color
-    // public
+    // Return a set of coordinates from a boat position
+    public Set<int[]> getPossibleMoves(int x, int y) {
+        Set<int[]> possibleMoves = new java.util.HashSet<int[]>();
+        Set<int[]> neighbors =  getNeighbors(x, y);
+
+        for(int[] neighbor : neighbors) {
+            // If the neighbor is iceberg, it is a possible move
+            if(this.get(neighbor[0], neighbor[1])) possibleMoves.add(neighbor);
+        }
+
+        if(possibleMoves.size() > 0) return possibleMoves;
+
+        // There is no close iceberg
+        // We need to find the closest iceberg
+
+        int searchDistance = 1;
+        final int maxSearchDistance = 9;
+        Set<int[][]> possiblePath = new java.util.HashSet<int[][]>();
+        Set<int[][]> validPath = new java.util.HashSet<int[][]>();
+        // Possible path : set of coordinates lists
+        // We explore a path and split it into possible moves
+
+        // Init possiblePath with the neighbors
+        for(int[] neighbor : neighbors) {
+            possiblePath.add(new int[][] {neighbor});
+        }
+
+        while(validPath.size() == 0 && searchDistance < maxSearchDistance) {
+            Set<int[][]> newPossiblePath = new java.util.HashSet<int[][]>();
+            for(int[][] path : possiblePath) {
+
+                int[] lastCoord = path[path.length - 1];
+                Set<int[]> newNeighbors = getNeighbors(lastCoord[0], lastCoord[1]);
+                for(int[] neighbor : newNeighbors) {
+                    // Check if the neighbor a boat
+                    if (neighbor[0] == x && neighbor[1] == y) {
+                        continue;
+                    }
+                    // TODO : Other boats
+
+                    // Check if the neighbor was already in the path
+                    if (this._coordInPath(neighbor, path)) {
+                        continue;
+                    }
+
+                    if(this.get(neighbor[0], neighbor[1])) {
+                        // If the neighbor is a iceberg, we found a possible move
+                        validPath.add(path);
+                    } else {
+                        // If the neighbor is not a iceberg, we add it to the new possible path
+                        int[][] newPath = new int[path.length + 1][];
+                        for(int i = 0; i < path.length; i++) {
+                            newPath[i] = path[i];
+                        }
+                        newPath[path.length] = new int[] {neighbor[0], neighbor[1]};
+                        newPossiblePath.add(newPath);
+                    }
+                }
+            }
+            possiblePath = newPossiblePath;
+            searchDistance++;
+        }
+
+        // Get the first moves of the valid paths
+        for(int[][] path : validPath) {
+            // Check that the move havent been added before
+            int[] firstMove = path[0];
+            if (!this._coordInPath(firstMove, possibleMoves)) {
+                possibleMoves.add(firstMove);
+            }
+        }
+        return possibleMoves;
+    }
+
+    private boolean _coordInPath(int[] coord, int[][] path) {
+        for(int[] pathCoord : path) {
+            if(pathCoord[0] == coord[0] && pathCoord[1] == coord[1]) return true;
+        }
+        return false;
+    }
+    private boolean _coordInPath(int[] coord,Set<int[]> path) {
+       return this._coordInPath(coord, path.toArray(new int[path.size()][]));
+    }
 }
